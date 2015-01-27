903b8a4bf86abd7fc31a6bf2f03de0c2c1e02b91
Marius Gundersen <gundersen@gmail.com>
2015-01-27 22:57:04 +0100

    Created a Virtually method for doing simple type based pattern matching. Really wish this was built into C#

---
+++ Qvc/Maybe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Qvc
+{
+    internal class Maybe<TInput, TResult>
+    {
+        private readonly TInput _input;
+        private TResult _result;
+
+        public Maybe(TInput input)
+        {
+            _input = input;
+        }
+
+        public Maybe<TInput, TResult> Case<T>(Func<T, TResult> match) where T : TInput
+        {
+            if (_input is T)
+            {
+                _result = match((T)_input);
+            }
+
+            return this;
+        }
+
+        public TResult Result()
+        {
+            return _result;
+        }
+    }
+
+    internal static class Maybe
+    {
+        public static Maybe<TInput, TResult> Virtually<TInput, TResult>(this TInput input)
+        {
+            return new Maybe<TInput, TResult>(input);
+        }
+    }
+}
